0b88214b19c68edb40908f86b60aa90aa85025c3
Karl Johansson <karl.johansson86@gmail.com>
2018-04-03 12:56:36 +0200

    Start of weather application

---
+++ README.md
@@ -0,0 +1,2 @@
+### Weather
+A program that locates you by your IP address and shows the weather at your location based on data from an the OpenWeatherMap
---
+++ src/main/java/com/karljohansson86/App.java
@@ -0,0 +1,14 @@
+package com.karljohansson86;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello Weather!" );
+
+
+    }
+}
